8537dd0362dd4c00294210f02f66231b6fa6c0ef
Leandro Perez <leandro.perez@celerative.com>
2020-09-13 11:42:28 -0300

    Created workspace and master detail project from template

---
+++ Reddit/Reddit/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Reddit
+//
+//  Created by Leandro Perez on 9/13/20.
+//  Copyright © 2020 Leandro Perez. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
