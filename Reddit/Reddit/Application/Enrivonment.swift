4ae2a32213abd3dce903bebc210c91553c34eeee
Leandro Perez <leandro.perez@celerative.com>
2020-09-21 00:51:08 -0300

    Implemented database using json file: 1. Added JSONFile 2. Added Database protocol and extension so JSONFile implements it 3. Configured environment 4. Added logic to interactor

--- Reddit/Reddit/Application/Enrivonment.swift
+++ Reddit/Reddit/Application/Enrivonment.swift
@@ -18,12 +18,15 @@ struct Environment {
     var coordinator : MainCoordinator!
     var imageCache : ImageCache!
     var redditAPI: RedditAPI
+    var redditDatabase : RedditDatabase
 
     internal init(coordinator: MainCoordinator? = MainCoordinator(),
                   imageCache : ImageCache = ImageCache(),
-                  redditAPI: RedditAPI = RedditAPI()) {
+                  redditAPI: RedditAPI = RedditAPI(),
+                  redditDatabase: RedditDatabase = JsonFile(name: "reddit.database")) {
         self.coordinator = coordinator
         self.imageCache = imageCache
         self.redditAPI = redditAPI
+        self.redditDatabase = redditDatabase
     }
 }
